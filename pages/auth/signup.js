35e29a6cfcd8de448354b054dfed232c190d40a1
Coda Bool <codabool@pm.me>
2021-04-26 01:51:33 -0400

    remove bcrypt import

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useForm, Controller } from 'react-hook-form'
-import bcrypt from 'bcryptjs'
+// import bcrypt from 'bcryptjs'
 import { Envelope, Key, Person } from 'react-bootstrap-icons'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
